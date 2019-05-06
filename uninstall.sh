d24769d801ef191d2d2c789e69aea6aa2e7899c2
Martin Ashby <martin@ashbysoft.com>
2019-05-06 22:12:56 +0100

    uninstall script improvement

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
+systemctl stop henrymail
+systemctl disable henrymail
 rm -rf /etc/henrymail /var/lib/henrymail /usr/local/bin/henrymail /etc/systemd/system/henrymail.service
 userdel henrymail
+systemctl daemon-reload
